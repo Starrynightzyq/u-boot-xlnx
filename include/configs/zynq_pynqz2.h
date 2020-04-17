1913c53340c6167d9afd23af407ca3f803146406
starrynightzyq <starrynightzyq@gmail.com>
2020-04-17 14:11:15 +0800

    pynq usb 0

---
+++ include/configs/zynq_pynqz2.h
@@ -0,0 +1,17 @@
+#ifndef __CONFIG_ZYNQ_PYNQZ2_H
+#define __CONFIG_ZYNQ_PYNQZ2_H
+
+#define CONFIG_ZYNQ_I2C0
+/*#define CONFIG_ZYNQ_I2C1
+#define CONFIG_SYS_I2C_EEPROM_ADDR_LEN  1
+#define CONFIG_ZYNQ_GEM_EEPROM_ADDR 0x50
+#define CONFIG_ZYNQ_GEM_I2C_MAC_OFFSET  0xFA
+#define CONFIG_DISPLAY
+#define CONFIG_I2C_EDID*/
+
+/* Define PYNQZ2 PS Clock Frequency to 50MHz */
+/* #define CONFIG_ZYNQ_PS_CLK_FREQ 50000000UL */
+
+#include <configs/zynq-common.h>
+
+#endif /* __CONFIG_ZYNQ_PYNQZ2_H */
